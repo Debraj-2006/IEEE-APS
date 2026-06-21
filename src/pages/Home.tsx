cc8ed9c50ba2627c9cd54f07f7c7425220129de3
Sumit Kumar Das <skdas5405@gmail.com>
2026-06-21 23:44:05 +0530

    Remove Conference from Initiatives and add to Navigation

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -761,15 +761,7 @@ export function Home() {
               link="/initiatives/event"
               index={4}
             />
-            <EventCard 
-              status="Active"
-              code="IN-CONFERENCE"
-              title="Conference"
-              date="UPCOMING"
-              image="https://images.unsplash.com/photo-1505373877841-8d25f7d46678?auto=format&fit=crop&q=80"
-              link="/initiatives/conference"
-              index={5}
-            />
+
           </div>
         </div>
       </section>
